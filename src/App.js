5d3bb7ebfb25f451bdc894b1c292571ef993a5b7
kosuke-17 <tamutamu17@icloud.com>
2021-05-24 13:41:02 +0900

    Card化して、Layoutで表示

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout >
       <Switch>
         <Route path="/" exact>
           <AllMeetupsPage />
@@ -22,7 +21,7 @@ function App() {
           <FavoritesPage />
         </Route>
       </Switch>
-    </div>
+    </Layout>
   );
 }
 
